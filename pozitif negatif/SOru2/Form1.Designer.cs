3bc2329a974927ec60ebd0e14eae0f866ada77df
Emirhanugrac19 <91061107+Emirhanugrac19@users.noreply.github.com>
2021-12-24 11:17:47 +0300

    TAMAMLANDI

---
+++ pozitif negatif/SOru2/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SOru2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.bes = new System.Windows.Forms.RadioButton();
+            this.yedi = new System.Windows.Forms.RadioButton();
+            this.uc = new System.Windows.Forms.RadioButton();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(184, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ">>AKTAR>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(27, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 47);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "OLUŞTUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(91, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "POZİTİF";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 68);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "NEGATİF";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // bes
+            // 
+            this.bes.AutoSize = true;
+            this.bes.Location = new System.Drawing.Point(30, 42);
+            this.bes.Name = "bes";
+            this.bes.Size = new System.Drawing.Size(31, 17);
+            this.bes.TabIndex = 2;
+            this.bes.TabStop = true;
+            this.bes.Text = "5";
+            this.bes.UseVisualStyleBackColor = true;
+            // 
+            // yedi
+            // 
+            this.yedi.AutoSize = true;
+            this.yedi.Location = new System.Drawing.Point(30, 65);
+            this.yedi.Name = "yedi";
+            this.yedi.Size = new System.Drawing.Size(31, 17);
+            this.yedi.TabIndex = 2;
+            this.yedi.TabStop = true;
+            this.yedi.Text = "7";
+            this.yedi.UseVisualStyleBackColor = true;
+            // 
+            // uc
+            // 
+            this.uc.AutoSize = true;
+            this.uc.Location = new System.Drawing.Point(30, 19);
+            this.uc.Name = "uc";
+            this.uc.Size = new System.Drawing.Size(31, 17);
+            this.uc.TabIndex = 2;
+            this.uc.TabStop = true;
+            this.uc.Text = "3";
+            this.uc.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(383, 65);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(91, 225);
+            this.listBox2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(184, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(90, 98);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.uc);
+            this.groupBox2.Controls.Add(this.bes);
+            this.groupBox2.Controls.Add(this.yedi);
+            this.groupBox2.Location = new System.Drawing.Point(184, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(90, 87);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 320);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton bes;
+        private System.Windows.Forms.RadioButton yedi;
+        private System.Windows.Forms.RadioButton uc;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ pozitif negatif/SOru2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOru2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox1.Items.Clear();
+            int rnd;
+            for (int i = 0; i < 100; i++)
+            {
+                rnd = random.Next(-100, 101);
+                listBox1.Items.Add(rnd);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                int a = (int)listBox1.Items[i];
+                Pozitif(a);
+                Negatif(a);
+            }
+        }
+
+        private void Pozitif(int a)
+        {
+            if (a > 0 && radioButton1.Checked)
+            {
+                üç(a);
+                beş(a);
+                yedı(a);
+            }
+        }
+
+        private void Negatif(int a)
+        {
+            if (a < 0 && radioButton2.Checked)
+            {
+                üç(a);
+                beş(a);
+                yedı(a);
+            }
+
+        }
+
+        private void yedı(int a)
+        {
+            if (a % 7 == 0 && yedi.Checked)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+
+        private void beş(int a)
+        {
+            if (a % 5 == 0 && bes.Checked)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+
+        private void üç(int a)
+        {
+            if (a % 3 == 0 && uc.Checked)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+    }
+}

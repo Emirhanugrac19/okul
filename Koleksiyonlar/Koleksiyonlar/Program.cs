98a2ecd3c61e28799748061ca699d5bb8a4991a1
Emirhanugrac19 <emirhanugrac19@gmail.com>
2021-12-31 09:30:09 +0300

    Yeniden Oluşturuldu

---
+++ Koleksiyonlar/Koleksiyonlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dizeler Örnekleri
+            // int[] sayilar = new int[5];
+            //string[] isimler = new string[5];
+            //bool[] durumlar = new bool[5];
+            //double[] kesirler = new double[5];
+            //sayilar[0] = 5;
+            //sayilar[1] = 3;
+            //sayilar[2] = 59;
+            //sayilar[3] = 55;
+            //sayilar[4] = 17;
+            //isimler[0] = "Utku";
+            //isimler[1] = "Arda";
+            //isimler[2] = "Furkan";
+            //isimler[3] = "Emir";
+            //isimler[4] = "Celal";
+            //durumlar[0] = true;
+            //durumlar[1] = true;
+            //durumlar[2] = true;
+            //durumlar[3] = false;
+            //durumlar[4] = false;
+            //kesirler[0] = 1.15;
+            //kesirler[1] = 1.16;
+            //kesirler[2] = 1.17;
+            //kesirler[3] = 1.18;
+            //kesirler[4] = 1.19;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Sayılar :  " + sayilar[i]);
+            //    Console.WriteLine("İsimler :  " + isimler[i]);
+            //    Console.WriteLine("Durumlar :  " + durumlar[i]);
+            //    Console.WriteLine("Kesirler :  " + kesirler[i]);
+
+            //}
+            //Console.ReadKey();
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}

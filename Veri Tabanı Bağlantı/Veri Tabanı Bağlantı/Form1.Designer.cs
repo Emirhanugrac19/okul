8ea4c23bb02f7c882c10d49dc12d3dbf53f003fe
Emirhanugrac19 <emirhanugrac19@gmail.com>
2022-03-14 11:24:22 +0300

    é

---
+++ Veri Tabanı Bağlantı/Veri Tabanı Bağlantı/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Veri_Tabanı_Bağlantı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.txtAnaAdi = new System.Windows.Forms.TextBox();
+            this.txtBabaAdi = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtPersonel_id = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(107, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(448, 206);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(146, 224);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtAdi.TabIndex = 1;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(146, 250);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyadi.TabIndex = 1;
+            // 
+            // txtAnaAdi
+            // 
+            this.txtAnaAdi.Location = new System.Drawing.Point(146, 276);
+            this.txtAnaAdi.Name = "txtAnaAdi";
+            this.txtAnaAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtAnaAdi.TabIndex = 1;
+            // 
+            // txtBabaAdi
+            // 
+            this.txtBabaAdi.Location = new System.Drawing.Point(146, 302);
+            this.txtBabaAdi.Name = "txtBabaAdi";
+            this.txtBabaAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtBabaAdi.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(271, 224);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(97, 40);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(374, 224);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(97, 40);
+            this.btnGüncelle.TabIndex = 2;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(477, 224);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(97, 40);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtPersonel_id
+            // 
+            this.txtPersonel_id.Location = new System.Drawing.Point(374, 290);
+            this.txtPersonel_id.Name = "txtPersonel_id";
+            this.txtPersonel_id.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonel_id.TabIndex = 3;
+            this.txtPersonel_id.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 347);
+            this.Controls.Add(this.txtPersonel_id);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtBabaAdi);
+            this.Controls.Add(this.txtAnaAdi);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.TextBox txtAnaAdi;
+        private System.Windows.Forms.TextBox txtBabaAdi;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtPersonel_id;
+    }
+}
+
---
+++ Veri Tabanı Bağlantı/Veri Tabanı Bağlantı/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Veri_Tabanı_Bağlantı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        DataTable dt;
+        OleDbCommand com;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridEkle();
+        }
+
+        private void gridEkle()
+        {
+            con = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source = personel1.accdb");
+            da = new OleDbDataAdapter("select * from Personel", con);
+            dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            com = new OleDbCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "insert into personel (Adi,Soyadi,Ana_adi,Baba_adi) values ('"+txtAdi.Text+"','"+txtSoyadi.Text+"','"+txtAnaAdi.Text+"','"+txtBabaAdi.Text+"')";
+            com.ExecuteNonQuery();
+            con.Close();
+            gridEkle();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            com = new OleDbCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "delete from Personel where Personel_id="+txtPersonel_id.Text+"";
+            com.ExecuteNonQuery();
+            con.Close();
+            gridEkle();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtPersonel_id.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtAdi.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSoyadi.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtAnaAdi.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtBabaAdi.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            com = new OleDbCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "update Personel set Adi='" + txtAdi.Text + "',Soyadi='" + txtSoyadi.Text + "',Ana_adi='" + txtAnaAdi.Text + "',Baba_adi='" +txtBabaAdi.Text+"' where Personel_id=" + txtPersonel_id.Text + "";
+            com.ExecuteNonQuery();
+            con.Close();
+            gridEkle();
+        }
+    }
+}
